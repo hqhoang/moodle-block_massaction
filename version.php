64933ebcf1801a6e8b40e687a0e650e1ba9a30a2
Huy Hoang <hoang027@umn.edu>
2011-10-12 12:09:53 -0500

    - added versioning info for submitting to Moodle.org

--- version.php
+++ version.php
@@ -1,2 +1,5 @@
 <?php
 $plugin->version = 2011081500;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'Mass Actions Block';
