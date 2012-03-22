d1ec968666b373ce3becbd3933c0ddbb7a758599
Huy Hoang <hoang027@umn.edu>
2012-03-22 11:18:41 -0500

    added French translation from Luiggi Sansonetti

---
+++ lang/fr/block_massaction.php
@@ -0,0 +1,56 @@
+<?php
+$string['pluginname'] = 'Bloc Actions en Lot';
+$string['massaction:use'] = 'Utiliser le bloc Actions en Lot';
+$string['blockname'] = 'Actions en Lot';
+$string['blocktitle'] = 'Actions en Lot';
+$string['usage'] = 'Aide Actions en Lot';
+
+$string['select'] = 'Selectionner';
+$string['selectall'] = 'Tout sélectionner';
+$string['itemsin'] = 'item(s)';
+$string['allitems'] = 'Sélectionner tous les items :';
+$string['deselect'] = 'Désélectionner';
+$string['deselectall'] = 'Tout désélectionner';
+$string['withselected'] = 'Pour ce qui est sélectionné';
+
+$string['action_moveleft'] = 'Diminuer le retrait (déplacer vers la gauche)';
+$string['action_moveright'] = 'Faire un retrait (déplacement vers la droite)';
+$string['action_hide'] = 'Masquer';
+$string['action_show'] = 'Montrer';
+$string['action_delete'] = 'Supprimer';
+$string['action_moveto'] = 'Déplacer';
+$string['action_movetosection'] = 'Déplacer dans la section :';
+
+$string['week'] = 'S';
+$string['topic'] = 'Section';
+$string['section'] = 'Section';
+$string['section_zero'] = 'Général';
+$string['selecttarget'] = 'Merci de sélectionner une section cible pour y déplacer des éléments';
+$string['noitemselected'] = 'Merci de sélectionner au moins un élément pour appliquer l\'action en lot';
+
+
+
+$string['noaction'] = 'Pas d\'action spécifiée';
+$string['invalidaction'] = 'Action inconnue : {$a}';
+$string['invalidmoduleid'] = 'Id de l\'élément invalide : {$a}';
+$string['invalidcoursemodule'] = 'Elément de cours invalide';
+$string['invalidcourseid'] = 'Id de cours invalide';
+$string['deletecheck'] = 'Confirmer la suppression en lot';
+$string['deletecheckpreconfirm'] = 'Etes-vous sûr de vouloir supprimer le(s) élément(s) suivant(s) ?';
+$string['deletecheckconfirm'] = 'Etes-vous VRAIMENT sûr de vouloir supprimer le(s) élément(s) suivant(s) ?';
+$string['sectionnotexist'] = 'La section cible n\'existe pas';
+$string['missingparam'] = 'Erreur de code : il manque le paramètre JSON "{$a}"';
+
+$string['usage_help'] = <<< EOB
+<p></p>
+<p> Ce bloc permet aux enseignants d'effectuer des actions sur les ressources multiples ou des activités dans l'espace de cours, plutôt que d'avoir à effectuer des actions répétées sur des éléments individuels. </p>
+<p> Pour utiliser ce bloc, Javascript doit être activé dans votre navigateur, vous devez être en mode édition dans la page d'accueil du cours. Seuls les cours au format HEBDOMADAIRE ou THEMATIQUE sont pris en charge. </p>
+<p>Les actions prises en charge sont : <br>
+<ul>
+<li>la suppression en lot</li>
+<li>le masquage en lot</li>
+<li>l'affichage en lot</li>
+<li>le déplacement en lot</li>
+</ul>
+Pour sélectionner un élément pour y effectuer une action, il suffit de cliquer sur la case à droite de celui-ci sur la page du cours, ou vous pouvez sélectionner tous les éléments, ou sélectionner tous les éléments dans une section à l'aide du bloc. Pour effectuer une action, cliquez sur l'action que vous souhaitez effectuer à l'intérieur du bloc. </p>
+EOB;
