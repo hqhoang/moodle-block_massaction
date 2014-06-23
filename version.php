9682a324b241ecc14d7f37d4e2c96de8be124c5c
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-06-23 09:43:27 +0100

    Version Bump

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013120600;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2014062300;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2012110900; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'Mass Actions Block';
